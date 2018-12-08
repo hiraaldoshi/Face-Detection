84d04c93fb4aa2647da53a103003c089f4e9b6a4
hiraaldoshi <hiraaldoshi@yahoo.com>
2018-12-07 22:03:05 -0600

    updating HAAR_Comparison

--- HAAR_Comparison.sv
+++ HAAR_Comparison.sv
@@ -9,6 +9,10 @@ output logic is_face
 );
 
 string feature_number;
+string x_coord;
+string y_coord;
+string width;
+string height;
 
 always_comb
 	begin
@@ -19,14 +23,21 @@ always_comb
 				for(int i= 0; i < 22; i++)
 					begin
 					
+						// format in form: STAGE_stage#_FEAT_NUM
 						feature_number = {"STAGE_", $sformatf("%d", i), "_FEAT_NUM"};
 						for(int j = 0; j < `feature_number; j++)
 							begin
 							
 								for(int k = 0; k < 3; k++)
 									begin
-									
-										if()
+										
+										// format in form: VARIABLE_stage#_feature#_rectangle#
+										x_coord = {"X_COORD_", $sformatf("%d", i), "_", $sformatf("%d", j), "_", $sformatf("%d", k)};
+										y_coord = {"Y_COORD_", $sformatf("%d", i), "_", $sformatf("%d", j), "_", $sformatf("%d", k)};
+										width = {"WIDTH_", $sformatf("%d", i), "_", $sformatf("%d", j), "_", $sformatf("%d", k)};
+										height = {"HEIGHT_", $sformatf("%d", i), "_", $sformatf("%d", j), "_", $sformatf("%d", k)};
+										
+										`ifdef x_coord					// make sure this is calling the macro definition, not the local string obj
 											begin
 											
 												
